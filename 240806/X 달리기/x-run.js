dd4e528e7f407b9a80364a8682c7aebed32a44d8
Sunkwan Kim <skyvlu@gmail.com>
2024-08-06 01:46:54 +0900

    Title: X 달리기, Time: 5067ms, Memory: 17MB, Status: Time Limit Exceed - CodeTree

---
+++ 240806/X 달리기/x-run.js
@@ -0,0 +1,19 @@
+const [n] = `${require('fs').readFileSync(0)}`.trim().split('\n').map(row => row.trim().split(' ').map(Number))
+
+let remain = n;
+let speed = 1;
+let time = 0;
+while(remain > 1) {
+    time += 1;
+    if (remain >= speed * 2) {
+        remain -= speed * 2;
+        speed += 1;
+        time += 1;
+    } else {
+        if (remain >= (speed - 1)) {
+            remain -= (speed - 1);
+        }
+    }
+}
+
+console.log(time);
